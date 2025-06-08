989372b0d519cc63b61be0f6d01b8922ed9d6515
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-07 22:06:40 -0500

    extend window interface

--- src/ts/noshboard.ts
+++ src/ts/noshboard.ts
@@ -10,4 +10,10 @@ function noshboard() {
     console.log(`config.debug = ${conf.debug}`)
 }
 
+declare global {
+    interface Window { noshboard: any }
+}
+
+window.noshboard = window.noshboard || {}
+
 window.noshboard = noshboard
