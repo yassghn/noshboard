d36132e1838eaf07be1dc378f4e545037be404dc
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-09 17:50:42 -0500

    add 2d canvas rendering interface

--- src/ts/noshboard/types.ts
+++ src/ts/noshboard/types.ts
@@ -1,3 +1,20 @@
 /**
  * types.ts
  */
+
+/**
+ * @typedef {object} CVS
+ * @type {CVS} 2d canvas rendering object
+ * @property {CanvasRenderingContext2D} ctx canvas 2d rendering context
+ * @property {object} api rendering api
+ * @property {number} width canvas width in pixels
+ * @property {number} height canvas height in pixels
+ */
+interface CVS {
+    ctx: CanvasRenderingContext2D
+    api: object
+    width: number
+    height: number
+}
+
+export type { CVS }
