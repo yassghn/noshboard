45352fa1f44cf6b0a98ebf4a14ac2a4c43b7c27a
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-09 18:43:27 -0500

    hew canvas stack

--- src/ts/noshboard/canvas.ts
+++ src/ts/noshboard/canvas.ts
@@ -2,8 +2,58 @@
  * canvas.ts
  */
 
-const canvas = {
+import api from '@noshboard/api'
+import type { CVS, CVS_STACK } from '@noshboard/types'
+import configJSON from '../../../resources/config.json' with { type: 'JSON' }
 
+function _hewCvsCtx(id: string): CanvasRenderingContext2D {
+    const cvsElem = document.getElementById(id) as HTMLCanvasElement
+    const ctx = cvsElem.getContext('2d') as CanvasRenderingContext2D
+    return ctx
+}
+
+function _hewCvs(id: string): CVS {
+    const ctx = _hewCvsCtx(id) as CanvasRenderingContext2D
+    const cvs: CVS = {
+        ctx: ctx,
+        api: api,
+        width: ctx.canvas.width,
+        height: ctx.canvas.height,
+    }
+    return cvs
+}
+
+function _hewBackgroundCvs(): CVS {
+    const id = configJSON.html.cvs.background.id
+    const cvs: CVS = _hewCvs(id)
+    return cvs
+}
+
+function _hewMessageCvs(): CVS {
+    const id = configJSON.html.cvs.message.id
+    const cvs: CVS = _hewCvs(id)
+    return cvs
+}
+
+function _hewForegroundCvs(): CVS {
+    const id = configJSON.html.cvs.foreground.id
+    const cvs: CVS = _hewCvs(id)
+    return cvs
+}
+
+function _hewCvsStack(): CVS_STACK {
+    const cvsStack: CVS_STACK = {
+        background: _hewBackgroundCvs(),
+        message: _hewMessageCvs(),
+        foreground: _hewForegroundCvs()
+    }
+    return cvsStack
+}
+
+const canvas = {
+    get cvsStack(): CVS_STACK {
+        return _hewCvsStack()
+    }
 }
 
 export default canvas
