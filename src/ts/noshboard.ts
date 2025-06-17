568d2fc631ca6a4465c44b8dabd0b8bb4e3dcaeb
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-17 16:01:57 -0500

    compose add to window. no "this" self execute wrapper.

--- src/ts/noshboard.ts
+++ src/ts/noshboard.ts
@@ -4,16 +4,32 @@
 
 import config from '@noshboard/config'
 
-function noshboard() {
-    const conf = config.obj
-    console.log('welcome to noshboard!')
-    console.log(`config.debug = ${conf.debug}`)
-}
-
+// extend Window interface, DOM api window, adding noshboard
 declare global {
-    interface Window { noshboard: any }
+    interface Window {
+        noshboard: any
+    }
 }
 
-window.noshboard = window.noshboard || {}
+(() => {
 
-window.noshboard = noshboard
+    /**
+     * noashboard main
+     */
+    function noshboard() {
+        const conf = config.obj
+        console.log('welcome to noshboard!')
+        console.log(`config.debug = ${conf.debug}`)
+    }
+
+    /**
+     * add noshboard function to DOM global space
+     */
+    function _addToWindow() {
+        window.noshboard = window.noshboard || {}
+        window.noshboard = noshboard
+    }
+
+    _addToWindow()
+
+})()
