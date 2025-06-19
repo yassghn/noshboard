b6a2a8f25387ab0e03f3e7d2f0aac07036234452
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-19 16:04:05 -0500

    fridge filler module

---
+++ src/ts/noshboard/strings.ts
@@ -0,0 +1,46 @@
+/**
+ * strings.ts
+ *
+ * @memberof noshboard
+ * @module noshboard/strings
+ * @property {noshboard.module:noshboard/strings} strings fetch strings from strings.json
+ */
+
+/**
+ * fetch strings.json
+ *
+ * @returns {Promise<object>} strings json object
+ */
+async function _fetchStringsJson(): Promise<object> {
+    const jsonPath = 'resources/strings.json'
+    const jsobObj = (await (await (fetch(jsonPath))).json())
+    return jsobObj
+}
+
+/**
+ * get noshboard strings object from json object
+ *
+ * @returns {Promise<object>} noshboard strings object
+ */
+async function _hewStrings(): Promise<object> {
+    const stringsJSON = await _fetchStringsJson()
+    const strings = { ...stringsJSON }
+    return strings
+}
+/**
+ * get noshboard strings object
+ *
+ * @returns {Promise<object>} noshboard strings object
+ */
+
+async function _getStrings(): Promise<object>  {
+    return _hewStrings()
+}
+
+const strings = {
+    async get(): Promise<object> {
+        return _getStrings()
+    }
+}
+
+export default strings
