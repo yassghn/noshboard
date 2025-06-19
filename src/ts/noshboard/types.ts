d9100084c046f9c2ccde3c890a6d1ed02bce3068
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-19 16:30:58 -0500

    full html config typing

--- src/ts/noshboard/types.ts
+++ src/ts/noshboard/types.ts
@@ -7,6 +7,39 @@
  */
 
 /**
+ * @type {CVS_CONFIG}
+ * @typedef {object} CVS_CONFIG
+ * @property {string} name name of canvas object
+ * @property {string} id canvas html element id
+ */
+interface CVS_CONFIG {
+    name: string
+    id: string
+}
+
+/**
+ * @type {CVS_STACK_CONFIG}
+ * @typedef {object} CVS_STACK_CONFIG
+ * @property {CVS_CONFIG} background background canvas config
+ * @property {CVS_CONFIG} message message canvas config
+ * @property {CVS_CONFIG} foreground foreground canvas config
+ */
+interface CVS_STACK_CONFIG {
+    background: CVS_CONFIG
+    message: CVS_CONFIG
+    foreground: CVS_CONFIG
+}
+
+/**
+ * @type {HTML_CONFIG}
+ * @typedef {object} HTML_CONFIG
+ * @property {object} cvs canvas stack configuration
+ */
+interface HTML_CONFIG {
+    cvs: CVS_STACK_CONFIG
+}
+
+/**
  * @type {CONFIG}
  * @typedef {object} CONFIG noshboard configuration interface
  * @property {boolean} debug debug flag
@@ -16,7 +49,7 @@
 interface CONFIG {
     debug: boolean
     verbose: boolean
-    html: object
+    html: HTML_CONFIG
 }
 
 /**
@@ -65,4 +98,4 @@ interface STORAGE {
     strings: STRINGS
 }
 
-export type { CONFIG, CVS, CVS_STACK, STRINGS, STORAGE }
+export type { CVS_CONFIG, CVS_STACK_CONFIG, HTML_CONFIG, CONFIG, CVS, CVS_STACK, STRINGS, STORAGE }
