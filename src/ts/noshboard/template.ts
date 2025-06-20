5df4df3560d96a7a198d0fe3bbd97cc88c1e847b
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-20 00:28:16 -0500

    check if template was added

--- src/ts/noshboard/template.ts
+++ src/ts/noshboard/template.ts
@@ -6,27 +6,44 @@
  * @property {noshboard.module:noshboard/template} template html templating
  */
 
+import storage from '@noshboard/storage'
+
 /**
  * check browser support for templates
  *
  * @returns {boolean} flag indicating support for html templates
  */
 function _templateIsSupported(): boolean {
-	if ('content' in document.createElement('template')) {
-		return true
-	}
-	return false
+    if ('content' in document.createElement('template')) {
+        return true
+    }
+    return false
+}
+
+/**
+ * check if bulletin template was already added
+ *
+ * @returns {boolean} flag indicating if bulletin template was appended
+ */
+function _bulletinTemplateAdded(): boolean {
+    const config = storage.config
+    const id = config.html.template.bulletin.templateId
+    const elem = document.getElementById(id)
+    if (elem) return true
+    return false
 }
 
 /**
  * append bulletin template to shadow dom
  */
 async function _appendBulletinTemplate() {
-    const path = 'resources/templates/bulletin.html'
-    const bulletinTemplate = (await(await fetch(path)).text())
-    const range = document.createRange()
-    const frag = range.createContextualFragment(bulletinTemplate)
-    document.body.appendChild(frag)
+    if (!_bulletinTemplateAdded()) {
+        const path = 'resources/templates/bulletin.html'
+        const bulletinTemplate = await (await fetch(path)).text()
+        const range = document.createRange()
+        const frag = range.createContextualFragment(bulletinTemplate)
+        document.body.appendChild(frag)
+    }
 }
 
 const template = {
