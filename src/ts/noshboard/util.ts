1d0cadbd24e31218bcf95340da0b8d06702743d2
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-21 22:52:50 -0500

    utility

---
+++ src/ts/noshboard/util.ts
@@ -0,0 +1,40 @@
+/**
+ * util.ts
+ *
+ * @memberof noshboard
+ * @module noshboard/util
+ * @property {noshboard.module:noshboard/window} util utility functions
+ */
+
+import html from '@noshboard/html'
+
+// extend Window interface, DOM api window, adding noshboard
+declare global {
+    interface Window {
+        noshboard: any
+    }
+}
+
+/**
+ * add noshboard function to DOM global space
+ */
+function _addToWindow(lambda: () => void) {
+    window.noshboard = window.noshboard || {}
+    window.noshboard = lambda
+}
+
+function _setResize() {
+    window.onresize = html.fitCvsStack
+}
+
+const util = {
+    addToWindow: (lambda: () => void) => {
+        _addToWindow(lambda)
+    },
+
+    setResize: () => {
+        _setResize()
+    }
+}
+
+export default util
