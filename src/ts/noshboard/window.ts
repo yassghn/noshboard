5855dd56d46b89b5c04fdfa3ce0d6cb09380dfd5
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-21 22:35:16 -0500

    add window module

---
+++ src/ts/noshboard/window.ts
@@ -0,0 +1,13 @@
+/**
+ * window.ts
+ *
+ * @memberof noshboard
+ * @module noshboard/window
+ * @property {noshboard.module:noshboard/window} window window element processing
+ */
+
+const window = {
+
+}
+
+export default window
