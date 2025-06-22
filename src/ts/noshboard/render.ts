a71b1a58b19f5a6e6d5af1f8d2db5a4f166e736f
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-21 22:22:47 -0500

    render module

---
+++ src/ts/noshboard/render.ts
@@ -0,0 +1,17 @@
+/**
+ * render.ts
+ *
+ * @memberof noshboard
+ * @module noshboard/render
+ * @property {noshboard.module:noshboard/render}
+ */
+
+function _render() {
+    
+}
+
+function render() {
+    _render()
+}
+
+export default render
