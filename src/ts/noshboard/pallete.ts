d01242fb9ccb7fdc44821d0e1c3816a2610e239c
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-06-23 17:54:55 -0500

    add pallete

---
+++ src/ts/noshboard/pallete.ts
@@ -0,0 +1,13 @@
+/**
+ * pallete.ts
+ *
+ * @memberof noshboard
+ * @module noshboard/pallete
+ * @property {noshboard.module:noshboard/pallete} pallete rendering palletes
+ */
+
+const pallete = {
+    
+}
+
+export default pallete
