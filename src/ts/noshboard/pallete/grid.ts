2f39e95b33137ed4e470a32a6d5eab5bc23e8c79
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-07-03 17:21:06 -0500

    debug grid

--- src/ts/noshboard/pallete/grid.ts
+++ src/ts/noshboard/pallete/grid.ts
@@ -10,26 +10,163 @@ import ctxState from '@noshboard/api/ctxState'
 import storage from '@noshboard/storage'
 import type { CVS } from '@noshboard/types'
 
-function _xGrid(cvs: CVS) {
-    const color = storage.config.noshboard.grid.lines.baseColor
-    const width = storage.config.noshboard.grid.lines.baseWidth
+/**
+ * render horizontal grid labels
+ *
+ * @param {CVS} cvs 2d canvas rendering object
+ */
+function _xLabels(cvs: CVS) {
+    const color = storage.config.noshboard.grid.labels.color
+    const width = storage.config.noshboard.grid.labels.width
+    const font = storage.config.noshboard.grid.labels.font
+    const fontSize = storage.config.noshboard.grid.labels.fontSize
     const state = ctxState.fresh()
-    state.props.fillStyle = `rgb(${color})`
+    state.props.strokeStyle = color
     state.props.lineWidth = parseInt(width)
+    state.props.font = `${fontSize} ${font}`
     state.apply((cvs) => {
-        for (let i = 0; i <= cvs.width; i++) {
-
+        const ctx = cvs.ctx
+        const spacing = parseInt(storage.config.noshboard.grid.labels.spacing)
+        const textColor = storage.config.noshboard.grid.labels.textColor
+        const fs = storage.config.noshboard.grid.labels.fontSize.split('px')[0] ?? '0'
+        const size = parseInt(fs)
+        for (let i = 0; i < cvs.width; i += spacing) {
+            // draw grid line
+            ctx.beginPath()
+            ctx.moveTo(i, 0)
+            ctx.lineTo(i, cvs.height)
+            ctx.stroke()
+            ctx.closePath()
+            // draw label
+            ctx.save()
+            ctx.strokeStyle = textColor
+            ctx.strokeText(`${i}`, i, size)
+            ctx.restore()
         }
     }, cvs)
 }
 
-function _yGrid(cvs: CVS) {}
+/**
+ *  render vertical grid lines
+ *
+ * @param {CVS} cvs 2d canvas rendering object
+ */
+function _xGrid(cvs: CVS) {
+    const color = storage.config.noshboard.grid.lines.baseColor
+    const width = storage.config.noshboard.grid.lines.baseWidth
+    const state = ctxState.fresh()
+    state.props.strokeStyle = color
+    state.props.lineWidth = parseInt(width)
+    state.apply((cvs) => {
+        const height = cvs.height
+        const ctx = cvs.ctx
+        const spacing = parseInt(storage.config.noshboard.grid.lines.baseSpacing)
+        for (let i = 0; i <= cvs.width; i += spacing) {
+            ctx.beginPath()
+            ctx.moveTo(i, 0)
+            ctx.lineTo(i, height)
+            ctx.stroke()
+            ctx.closePath()
+        }
+    }, cvs)
+}
 
-function _centerHighlight(cvs: CVS) {}
+/**
+ * render vertical grid labels
+ *
+ * @param {CVS} cvs 2d canvas rendering object
+ */
+function _yLabels(cvs: CVS) {
+    const color = storage.config.noshboard.grid.labels.color
+    const width = storage.config.noshboard.grid.labels.width
+    const font = storage.config.noshboard.grid.labels.font
+    const fontSize = storage.config.noshboard.grid.labels.fontSize
+    const state = ctxState.fresh()
+    state.props.strokeStyle = color
+    state.props.lineWidth = parseInt(width)
+    state.props.font = `${fontSize} ${font}`
+    state.apply((cvs) => {
+        const ctx = cvs.ctx
+        const spacing = parseInt(storage.config.noshboard.grid.labels.spacing)
+        const textColor = storage.config.noshboard.grid.labels.textColor
+        for (let i = 0; i < cvs.height; i += spacing) {
+            // draw grid line
+            ctx.beginPath()
+            ctx.moveTo(0, i)
+            ctx.lineTo(cvs.width, i)
+            ctx.stroke()
+            ctx.closePath()
+            // draw label
+            ctx.save()
+            ctx.strokeStyle = textColor
+            ctx.strokeText(`${i}`, 0, i)
+            ctx.restore()
+        }
+    }, cvs)
+}
 
+/**
+ *  render horizontal grid lines
+ *
+ * @param {CVS} cvs 2d canvas rendering object
+ */
+function _yGrid(cvs: CVS) {
+    const color = storage.config.noshboard.grid.lines.baseColor
+    const width = storage.config.noshboard.grid.lines.baseWidth
+    const state = ctxState.fresh()
+    state.props.strokeStyle = color
+    state.props.lineWidth = parseInt(width)
+    state.apply((cvs) => {
+        const width = cvs.width
+        const ctx = cvs.ctx
+        const spacing = parseInt(storage.config.noshboard.grid.lines.baseSpacing)
+        for (let i = 0; i <= cvs.width; i += spacing) {
+            ctx.beginPath()
+            ctx.moveTo(0, i)
+            ctx.lineTo(width, i)
+            ctx.stroke()
+            ctx.closePath()
+        }
+    }, cvs)
+}
+
+/**
+ *  highlight grid center
+ *
+ * @param {CVS} cvs 2d canvas rendering object
+ */
+function _centerHighlight(cvs: CVS) {
+    const color = storage.config.noshboard.grid.lines.centerColor
+    const width = storage.config.noshboard.grid.lines.centerWidth
+    const state = ctxState.fresh()
+    state.props.strokeStyle = color
+    state.props.lineWidth = parseInt(width)
+    state.apply((cvs) => {
+        const ctx = cvs.ctx
+        const center = cvs.api.browserWindow.center
+        ctx.beginPath()
+        ctx.moveTo(center.x, 0)
+        ctx.lineTo(center.x, cvs.height)
+        ctx.stroke()
+        ctx.closePath()
+        ctx.beginPath()
+        ctx.moveTo(0, center.y)
+        ctx.lineTo(cvs.width, center.y)
+        ctx.stroke()
+        ctx.closePath()
+    }, cvs)
+}
+
+/**
+ *  grid pallete to render
+ *
+ * @param {CVS} cvs 2d canvas rendering object
+ */
 function _gridPallete(cvs: CVS) {
     _xGrid(cvs)
     _yGrid(cvs)
+    _xLabels(cvs)
+    _yLabels(cvs)
     _centerHighlight(cvs)
 }
 
