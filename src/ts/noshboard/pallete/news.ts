3d7328ffae54d37d7432584472a7a60f69a0345e
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-07-02 23:28:49 -0500

    theme

--- src/ts/noshboard/pallete/news.ts
+++ src/ts/noshboard/pallete/news.ts
@@ -6,12 +6,13 @@
  * @property {noshboard.module:noshboard/pallete/news}
  */
 
-import type { CVS } from '../types';
+import theme from '@noshboard/theme'
+import type { CVS } from '../types'
 
 function _newsPallete(cvs: CVS) {
     const ctxState = cvs.api.ctxState.fresh()
     ctxState.props.lineWidth = 5
-    ctxState.props.fillStyle = `rgb(185, 13, 13)`
+    ctxState.props.fillStyle = theme.obj.secondaryColor
     ctxState.apply((cvs) => {
         const ctx = cvs.ctx
         const center = cvs.api.browserWindow.center
