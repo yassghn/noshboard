19ba8991a80bc79937b5d189358a54ce0a624ac6
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-07-06 13:43:25 -0500

    new effects module

---
+++ src/ts/noshboard/effects.ts
@@ -0,0 +1,13 @@
+/**
+ * effects.ts
+ *
+ * @memberof noshboard
+ * @module noshboard/effects
+ * @property {noshboard.module:noshboard/effects} effects canvas effects
+ */
+
+const effects = {
+
+}
+
+export default effects
