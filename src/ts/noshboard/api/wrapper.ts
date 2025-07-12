2e49626502cec1395078c0cc6af8b2783c9d969a
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-07-12 14:14:45 -0500

    add rendering context 2d api wrapper

---
+++ src/ts/noshboard/api/wrapper.ts
@@ -0,0 +1,13 @@
+/**
+ * wrapper.ts
+ *
+ * @memberof noshboard/api
+ * @module noshboard/api/wrapper
+ * @property {noshboard.module:noshboard/api/wrapper}
+ */
+
+const wrapper = {
+
+}
+
+export default wrapper
