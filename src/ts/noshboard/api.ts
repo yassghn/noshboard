94e215d4a4df715d98ef04180123d794726dd3ec
YASSGHN * KATYUSHA <yassghn@protonmail.com>
2025-07-12 14:22:16 -0500

    no clip debug

--- src/ts/noshboard/api.ts
+++ src/ts/noshboard/api.ts
@@ -8,11 +8,14 @@
 
 import browserWindow from '@noshboard/api/browserWindow'
 import ctxState from '@noshboard/api/ctxState'
+import wrapper from '@noshboard/api/wrapper'
 import type { BROWSER_WINDOW_API,  } from './types'
 
 const api = {
     browserWindow: browserWindow as BROWSER_WINDOW_API,
-    ctxState: ctxState
+    ctxState: ctxState,
+    // canvas rendering context 2d api wrapper
+    clip: wrapper.clip
 }
 
 export default api
--- src/ts/noshboard/api/wrapper.ts
+++ src/ts/noshboard/api/wrapper.ts
@@ -6,8 +6,21 @@
  * @property {noshboard.module:noshboard/api/wrapper} wrapper canvas 2d rendering context 2d api wrapper
  */
 
-const wrapper = {
+import state from '@noshboard/state'
 
+/**
+ * no clip debug
+ *
+ * @param {CanvasRenderingContext2D} ctx canvas 2d rendering context
+ */
+function _clip(ctx: CanvasRenderingContext2D) {
+    if (!state.debug) {
+        ctx.clip()
+    }
+}
+
+const wrapper = {
+    clip: _clip
 }
 
 export default wrapper
